1629e280bfc090088597bf9532e2f606cc8bb1fe
Sunho Ju <sunho5515@gmail.com>
2024-04-05 20:34:26 +0900

    글목록 만들기 완료

---
+++ src/components/BoardList.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h5>{{dataList.title}}</h5>
+    <p>{{ dataList.content }}</p>
+    <p>{{ dataList.date }}</p>
+    <p>{{ dataList.number }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BoardList",
+  props:{
+    dataList: Array,
+  }
+}
+</script>
+
+<style>
+
+</style>
